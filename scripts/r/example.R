72aadb674293a001985756ef7cdd73c818526042
hemprichbennett <hemprich.bennett@gmail.com>
2018-08-29 18:36:21 +0100

    Made repo

---
+++ scripts/r/example.R
@@ -0,0 +1,7 @@
+##################################################
+## Project:
+## Script purpose:
+## Date:
+## Author: Dave Hemprich-Bennett (hemprich.bennett@gmail.com)
+## Notes
+##################################################
