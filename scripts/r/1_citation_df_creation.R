241631682b78947d7eb74a8b7e46c665776c4845
hemprichbennett <hemprich.bennett@gmail.com>
2018-08-31 10:57:32 +0100

    Made a csv from all bib files

---
+++ scripts/r/1_citation_df_creation.R
@@ -0,0 +1,22 @@
+#### Header ####
+## Project: editor_networks
+## Script purpose: merging together the bib files into a useable df
+## Date: 31/08/18
+## Author: Dave Hemprich-Bennett (hemprich.bennett@gmail.com)
+## Notes
+##################################################
+
+library(here)
+library(bibliometrix)
+setwd(here())
+
+####Load in the files ####
+
+filenames <- paste('data/raw_data/', list.files(path = 'data/raw_data/', pattern = 'bib'), sep = '')
+
+file_list <- lapply(filenames, readFiles)
+files <- lapply(file_list, function(x) convert2df(x, dbsource = "scopus", format = "bibtex"))
+
+df <- do.call(rbind, files)
+
+write.csv(df, 'data/processed_data/citation_df.csv')
