b804b4c986c7e4d44c3c5493a00e4ac1c641d158
hemprichbennett <hemprich.bennett@gmail.com>
2018-08-31 15:42:50 +0100

    Added code plotting how our surnames have (apparently) published together less assortatively over time

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 Scopus bib files were downloaded using the terms TITLE-ABS-KEY ( ecology,conservation )  AND  DOCTYPE ( ar  OR  re )  AND  ( LIMIT-TO ( SRCTYPE ,  "j" ) ), and then splitting the results by year and country as required to stay under the limit of 2,000 records per download
+
+
+![Example figure showing how our surnames assortment in publishing has decreased over time](figures/example_authors.jpg)
--- scripts/2_networks.R
+++ scripts/2_networks.R
@@ -9,6 +9,7 @@
 library(here)
 library(bibliometrix)
 library(igraph)
+library(ggplot2)
 
 setwd(here())
 
@@ -20,14 +21,49 @@ auths <-  biblioNetwork(df, analysis = "collaboration", network = "authors", sep
 
 auth_net <- graph_from_adjacency_matrix(auths, mode = 'undirected')
 
-#A trial, give the nodes a value indicating if they're a Bennett or a Caravaggi
+#A trial, give the nodes a value indicating if they're a Bennett, Caravaggi or Bond
 
 V(auth_net)$dummy <- 'non'
 
-desired_names <- c('BENNETT', 'CARAVAGGI')
+desired_names <- c('BENNETT', 'CARAVAGGI', 'BOND')
 V(auth_net)$dummy[grep(paste(desired_names,collapse="|"), V(auth_net)$name)] <- 'badass'
 
 V(auth_net)$dummy <- as.factor(V(auth_net)$dummy)
 
 assortativity_nominal(auth_net, V(auth_net)$dummy, directed=F)
 #Low assortivity is detected between our surnames
+
+
+#how does this vary over time?
+
+dates <- c()
+assorts <- c()
+
+for(i in 1:length(unique(df$PY))){
+  year <- unique(df$PY)[i]
+  
+  temp_net <- graph_from_adjacency_matrix(biblioNetwork(df[df$PY == year,], analysis = "collaboration", network = "authors", sep = ";"),  mode = 'undirected')
+  
+  V(temp_net)$dummy <- 'non'
+  
+  
+  V(temp_net)$dummy[grep(paste(desired_names,collapse="|"), V(temp_net)$name)] <- 'badass'
+  
+  V(temp_net)$dummy <- as.factor(V(temp_net)$dummy)
+  
+  score <- assortativity_nominal(temp_net, V(temp_net)$dummy, directed=F)
+  
+  dates <- c(dates, year)
+  assorts <- c(assorts, score)
+}
+
+assort_df <- data.frame(dates, assorts)
+
+dummy_plot <- ggplot(assort_df, aes(x = dates, y= assorts))+ 
+  xlab('Year')+ ylab('Assortment between Bennetts, Bonds and Caravaggis\nin ecology and conservation publishing')+
+  geom_point()+ 
+  theme_bw() + theme(panel.border = element_blank(), panel.grid.major = element_blank(),panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))
+
+jpeg('figures/dummy_plot.jpg', units = 'in', width = 9, height = 9, res=300)
+dummy_plot
+dev.off()
