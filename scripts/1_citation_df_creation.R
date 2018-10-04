dc889a003dd986436e602704a96566409fcc6708
hemprichbennett <hemprich.bennett@gmail.com>
2018-10-04 14:39:02 +0100

    Altered script for web of science data (used to be scopus)

--- scripts/1_citation_df_creation.R
+++ scripts/1_citation_df_creation.R
@@ -12,11 +12,29 @@ setwd(here())
 
 ####Load in the files ####
 
-filenames <- paste('data/raw_data/scopus_bibs/', list.files(path = 'data/raw_data/scopus_bibs/', pattern = 'bib'), sep = '')
+filenames <- paste('data/raw_data/wos/', list.files(path = 'data/raw_data/wos/', pattern = 'bib'), sep = '')
 
 file_list <- lapply(filenames, readFiles)
-files <- lapply(file_list, function(x) convert2df(x, dbsource = "scopus", format = "bibtex"))
+files <- lapply(file_list, function(x) convert2df(x, dbsource = "isi", format = "bibtex"))
+
+
+#Make a vector of all the column names, then a vector of all the column names which are found in every file (there are a few files with redundant
+#columns that don't appear elsewhere, killing the rbind)
+all_colnames <- unlist(sapply(files, function(x) colnames(x)))
+goodcols <- c()
+for(i in 1:length(unique(all_colnames))){
+  nam <- unique(all_colnames)[i]
+  if(length(which(all_colnames == nam)) == length(files)){
+    goodcols <- c(goodcols, nam)
+  }
+}
+
+#now discard any unwanted columns
+for(i in 1:length(files)){
+  files[[i]] <- files[[i]][,which(colnames(files[[i]]) %in% goodcols)]
+}
 
 df <- do.call(rbind, files)
 
 write.csv(df, 'data/processed_data/citation_df.csv')
+
